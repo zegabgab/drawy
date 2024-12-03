a5144bddbb1963315233807746c32258ed4473b6
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-12-03 22:21:33 +0100

    Deleted everything :)

--- JDrawy/src/main/java/com/gmail/gabrielkorherr/DrawOperation.java
+++
@@ -1,6 +0,0 @@
-package com.gmail.gabrielkorherr;
-
-@FunctionalInterface
-public interface DrawOperation {
-    void applyTo(Surface surface);
-}
--- JDrawy/src/main/java/com/gmail/gabrielkorherr/Surface.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.gabrielkorherr;
-
-/**
- * An interface to draw on surfaces like a pixel buffer
- */
-public interface Surface {
-    void drawPoint(int x, int y);
-}
